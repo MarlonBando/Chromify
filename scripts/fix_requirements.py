6df70f59d822025ce06c03834c8a225f37a062bd
MarlonBando <michele.bandini2000@gmail.com>
2025-01-20 16:04:08 +0100

    updated automatically requirements.txt

--- scripts/fix_requirements.py
+++ scripts/fix_requirements.py
@@ -1,24 +1,25 @@
 def fix_requirements():
-    '''
+    """
     When we run pipreqs it adds 'skimage==0.0' to requirments.txt
     This create issue. This script automatically replace 'skimage==0.0'
     with scikit-image==0.22.0
-    '''
+    """
     try:
-        with open('requirements.txt', 'r') as file:
+        with open("requirements.txt", "r") as file:
             requirements = file.read()
-        
-        updated_requirements = requirements.replace('skimage==0.0', 'scikit-image==0.22.0')
-        
-        with open('requirements.txt', 'w') as file:
+
+        updated_requirements = requirements.replace("skimage==0.0", "scikit-image==0.22.0")
+
+        with open("requirements.txt", "w") as file:
             file.write(updated_requirements)
-            
+
         print("Successfully updated requirements.txt")
-        
+
     except FileNotFoundError:
         print("Error: requirements.txt not found")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
 
 if __name__ == "__main__":
     fix_requirements()
