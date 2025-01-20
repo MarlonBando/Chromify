3f99a0a447611bd4c9c33f62e1c16e00099506e9
Samer <samerbuj@gmail.com>
2025-01-20 16:38:45 +0100

    added some tests for utils file

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import os
+import torch
+import numpy as np
+import chromify.utils as utils
+
+def test_get_project_root():
+    assert os.path.exists(utils.get_project_root())
+    assert os.path.basename(utils.get_project_root()) == ("Chromify" or "chromify")
+    assert os.path.isdir(os.path.join(utils.get_project_root(), "data"))
+    assert os.path.isdir(os.path.join(utils.get_project_root(), "tests"))
+    assert os.path.isdir(os.path.join(utils.get_project_root(), "src"))
+
+def test_lab_to_rgb():
+    # this function turns a batch of L*a*b* images into a batch of RGB images
+    L = torch.randn(1, 1, 256, 256)
+    ab = torch.randn(1, 2, 256, 256)
+    rgb_imgs = utils.lab_to_rgb(L, ab)
+    assert rgb_imgs.shape == (1, 256, 256, 3)
+    assert rgb_imgs.dtype == np.float32
+    assert False
