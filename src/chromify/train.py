e3f29709959ec42aed6be763e0a83cd467d67af2
samerbuj <samerbuj@gmail.com>
2025-01-21 14:21:19 +0100

    adding functionalities for toml

--- src/chromify/train.py
+++ src/chromify/train.py
@@ -1,32 +1,24 @@
 import os
-import sys
 
 import torch
-import torch.nn as nn
-from model import MainModel
+from src.chromify.model import MainModel
 from tqdm import tqdm
+import argparse
 from src.chromify.utils import create_loss_meters, get_project_root, log_results, update_losses
+from src.chromify.data import make_dataloaders
 
-from data import make_dataloaders
+def parse_args():
+    """Parse command-line arguments"""
+    parser = argparse.ArgumentParser(description="Train the model")
+    parser.add_argument(
+        "epochs", type=int, nargs="?", default=10, help="Number of epochs to train the model"
+    )
+    return parser.parse_args()
 
-sys.path.insert(0, os.path.abspath("."))
+def train_model(model, data_dir, epochs, display_every=200):
+    train_dl = make_dataloaders(path=data_dir, split="train")
+    val_dl = make_dataloaders(path=data_dir, split="val")
 
-# Define the device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Define the model, loss function, and optimizer
-model = MainModel()
-model = model.to(device)
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-data_dir = os.path.join(get_project_root(), "data", "raw")
-
-train_dl = make_dataloaders(path=data_dir, split="train")
-val_dl = make_dataloaders(path=data_dir, split="val")
-
-
-def train_model(model, train_dl, epochs, display_every=200):
     data = next(iter(val_dl))  # getting a batch for visualizing the model output after fixed intrvals
     for e in range(epochs):
         loss_meter_dict = create_loss_meters()  # function returing a dictionary of objects to
@@ -37,13 +29,24 @@ def train_model(model, train_dl, epochs, display_every=200):
             update_losses(model, loss_meter_dict, count=data["L"].size(0))  # function updating the log objects
             i += 1
             if i % display_every == 0:
-                print(f"\nEpoch {e+1}/{epochs}")
-                print(f"Iteration {i}/{len(train_dl)}")
+                print(f"\nEpoch {e + 1} / {epochs}")
+                print(f"Iteration {i} / {len(train_dl)}")
                 log_results(loss_meter_dict)  # function to print out the losses
                 # visualize(model, data, save=False) # function displaying the model's outputs
 
+def main():
+    args = parse_args()
 
-model = MainModel()
+    # Define the device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Define the model, loss function, and optimizer
+    model = MainModel()
+    model = model.to(device)
+
+    data_dir = os.path.join(get_project_root(), "data", "raw")
+    
+    train_model(model, data_dir, epochs=args.epochs)
 
 if __name__ == "__main__":
-    train_model(model, train_dl, 10)
+    main()
