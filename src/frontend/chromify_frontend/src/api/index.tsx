99f96a2ba01b03b9491ccf1ade823abda0ad2e7e
pierGit7 <sorgiovanni60@gmail.com>
2025-01-23 17:12:03 +0100

    Refactor image upload functionality and enhance API response handling in frontend

--- src/frontend/chromify_frontend/src/api/index.tsx
+++ src/frontend/chromify_frontend/src/api/index.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
-
+import { ApiResponseColorizedImage } from './interface';
 const API_URL = 'http://0.0.0.0:8000/infer';
 
 export const callApi = async (data: any) => {
     try {
-        const response = await axios.post(API_URL, data);
+        const response = await axios.post<ApiResponseColorizedImage>(API_URL, data);
+        console.log('API response:', response.data);
         return response.data;
     } catch (error) {
         console.error('Error calling API:', error);
